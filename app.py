d94474528c0213fadfc13cefa92d200fd6572940
Karl Ágústsson <kag@tskoli.is>
2018-04-05 07:59:19 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,102 @@
+from bottle import run, route, template, request, response, redirect, app
+from beaker.middleware import SessionMiddleware
+
+#Einfalt cookie sýnidæmi
+'''
+@route('/')
+def index():
+    if request.get_cookie('hello'):
+        return "Hello again"
+    else:
+        response.set_cookie('hello', 'world')
+        return "Hello world..."
+'''
+
+#Login lausn - cookie
+adminuser = 'admin'
+adminpwd = '12345'
+
+@route('/')
+def index():
+    return template('index')
+
+@route('/login')
+def login():
+    return template('login')
+
+@route('/login', method='post')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    if username == adminuser and password == adminpwd:
+        response.set_cookie('account', username, secret='my_secret_code')
+        return redirect('/restricted')
+    else:
+        return "Login failed. <br> <a href='/login'>Login</a>"
+
+@route('/restricted')
+def restricted():
+    user = request.get_cookie('account', secret='my_secret_code')
+    print(user)
+    if(user):
+        return "Restricted area. <br>" \
+               " <a href='/signout'>Log off</a>"
+    else:
+        return "You are not logged in. Access denied."
+
+@route('/signout')
+def signout():
+    response.set_cookie('account', "", expires=0)
+    return "You have been signed out." \
+    "<br> <a href='/login'>Log in</a>"
+#--------
+
+#Session lausn
+
+session_options = {
+    'session.type': 'file',
+    'session.data_dir': './data'
+}
+
+my_session = SessionMiddleware(app(), session_options)
+
+products = [
+    {"pid": 1, "name": "Vara A", "price": 100},
+    {"pid": 2, "name": "Vara B", "price": 400},
+    {"pid": 3, "name": "Vara C", "price": 800},
+    {"pid": 4, "name": "Vara D", "price": 300}
+]
+
+@route('/shop')
+def shop():
+    return template('shop', products=products)
+
+@route('/cart/add/<id>')
+def add_to_cart(id):
+    session = request.environ.get('beaker.session')
+    session[id] = products[int(id)-1]['name']
+    session.save()
+
+    print(session)
+    return redirect('/cart')
+
+@route('/cart')
+def cart():
+    session = request.environ.get('beaker.session')
+    karfa = []
+    for i in range(1, len(products)+1):
+        i = str(i)
+        if session.get(i):
+            vara = session.get(i)
+            karfa.append(vara)
+
+    return template('cart', karfa=karfa)
+
+@route('/cart/remove')
+def remove_cart():
+    session = request.environ.get('beaker.session')
+    session.delete()
+    return redirect('/shop')
+
+run(app=my_session, port=5000)
---
+++ cart.tpl
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Karfan þín</title>
+</head>
+<body>
+
+    <h2>Karfan þín:</h2>
+
+    % if len(karfa) <= 0:
+        <p>Karfan er tóm.</p>
+        <p><a href="/shop">Aftur í verslun</a></p>
+    % else:
+    %   for i in karfa:
+            <p>{{i}}</p>
+    %   end
+        <p><a href="/shop">Aftur í verslun</a></p>
+    % end
+
+    <p><a href="/cart/remove">Tæma körfu</a></p>
+
+
+</body>
+</html>
